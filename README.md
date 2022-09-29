00aac0cdba1583d486709c41df58de57f27e273e
Joy Okebugwu <okebugwujoy@gmail.com>
2022-09-29 04:43:08 -0700

    initial commit -project complete

---
+++ README.md
@@ -0,0 +1,2 @@
+# easybank-landing-page
+# easybank-landing-page
---
+++ room-homepages-yt/src/App.js
@@ -0,0 +1,16 @@
+
+import Header from "./components/Header"
+import Showcase from "./components/Showcase"
+import SectionTwo from "./components/SectionTwo"
+
+function App() {
+  return (
+   <>
+    <Header/>
+    <Showcase/>
+    <SectionTwo/>
+   </>
+  );
+}
+
+export default App;
